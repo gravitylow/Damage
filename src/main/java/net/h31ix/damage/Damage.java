d694907b5e5924ad59da84cfc625c17282e27464
H31IX <effectsdude@gmail.com>
2012-06-04 21:48:37 -0400

    Code, yay.

---
+++ src/main/java/net/h31ix/damage/Damage.java
@@ -0,0 +1,95 @@
+package net.h31ix.damage;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Damage extends JavaPlugin implements Listener, CommandExecutor 
+{
+    Map<String,Double> damage = new HashMap<String,Double>();
+    
+    @Override
+    public void onDisable() 
+    {
+    }
+    
+    @Override
+    public void onEnable() 
+    {
+        File file = new File(this.getDataFolder()+"/config.yml");
+        if(!file.exists()) {
+            this.saveDefaultConfig();
+        }
+        getServer().getPluginManager().registerEvents(this, this);
+        List<String> list = (List<String>)getConfig().getList("permissions");
+        for(String string : list)
+        {
+            String [] s = string.split(":");
+            damage.put(s[0],Double.parseDouble(s[1]));
+        }    
+        getCommand("damage").setExecutor(this);
+    }
+    
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent event) 
+    {
+        if(event.getEntity() instanceof Player)
+        {
+            Player player = (Player)event.getEntity();
+            double d = -1;
+            for(String string : damage.keySet())
+            {
+                if(player.hasPermission(string))
+                {
+                    d = damage.get(string);
+                    break;
+                }
+            }
+            if(d != -1)
+            {
+                if(event.getEntity() instanceof Player && !(event.getDamager() instanceof Player) && event.getDamager() instanceof LivingEntity) {
+                    event.setDamage((int)(d*event.getDamage()));
+                }
+                else if (event.getEntity() instanceof Player && event.getDamager() instanceof Arrow && !(((Arrow)event.getDamager()).getShooter() instanceof Player) && ((Arrow)event.getDamager()).getShooter() instanceof LivingEntity) {
+                    event.setDamage((int)(d*event.getDamage()));
+                }
+            }
+        }
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender cs, Command cmd, String alias, String[] args) 
+    {
+        if(args.length == 1)
+        {
+            if(args[0].equalsIgnoreCase("reload"))
+            {
+                if(cs.hasPermission("damage.reload"))
+                {
+                    this.reloadConfig();
+                    List<String> list = (List<String>)getConfig().getList("permissions");
+                    for(String string : list)
+                    {
+                        String [] s = string.split(":");
+                        damage.put(s[0],Double.parseDouble(s[1]));
+                    } 
+                    cs.sendMessage(ChatColor.GREEN+"Damage configuration reloaded.");
+                }
+            }
+        }
+        return true;
+    }
+}
+
