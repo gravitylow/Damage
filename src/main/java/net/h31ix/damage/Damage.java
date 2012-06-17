63e8cb46e609776a6e1c366e375175f5bc480c50
H31IX <effectsdude@gmail.com>
2012-06-17 17:34:38 -0400

    Modify XP as well.

--- src/main/java/net/h31ix/damage/Damage.java
+++ src/main/java/net/h31ix/damage/Damage.java
@@ -9,16 +9,19 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Damage extends JavaPlugin implements Listener, CommandExecutor 
 {
     Map<String,Double> damage = new HashMap<String,Double>();
+    Map<String,Integer> xp = new HashMap<String,Integer>();
     
     @Override
     public void onDisable() 
@@ -33,12 +36,18 @@ public class Damage extends JavaPlugin implements Listener, CommandExecutor
             this.saveDefaultConfig();
         }
         getServer().getPluginManager().registerEvents(this, this);
-        List<String> list = (List<String>)getConfig().getList("permissions");
+        List<String> list = getConfig().getStringList("damage");
         for(String string : list)
         {
             String [] s = string.split(":");
             damage.put(s[0],Double.parseDouble(s[1]));
-        }    
+        }  
+        list = getConfig().getStringList("xp");
+        for(String string : list)
+        {
+            String [] s = string.split(":");
+            xp.put(s[0],Integer.parseInt(s[1]));
+        }          
         getCommand("damage").setExecutor(this);
     }
     
@@ -65,6 +74,29 @@ public class Damage extends JavaPlugin implements Listener, CommandExecutor
                 else if (event.getEntity() instanceof Player && event.getDamager() instanceof Arrow && !(((Arrow)event.getDamager()).getShooter() instanceof Player) && ((Arrow)event.getDamager()).getShooter() instanceof LivingEntity) {
                     event.setDamage((int)(d*event.getDamage()));
                 }
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event)
+    {
+        Entity e = event.getEntity().getLastDamageCause().getEntity();
+        if(e != null && e instanceof Player)
+        {
+            Player player = (Player)e;
+            int d = -1;
+            for(String string : xp.keySet())
+            {
+                if(player.hasPermission(string))
+                {
+                    d = xp.get(string);
+                    break;
+                }
+            }
+            if(d != -1)
+            {            
+                event.setDroppedExp((int)(event.getDroppedExp()*d));
             }
         }
     }
